a6523caf8f3ca16bb1ceb01c2b036c0a45b652f7
leostudioshop1 <leostudioshop1@gmail.com>
2026-03-29 12:47:05 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-<script>
-
-// 🔐 dados diretos (sem base64)
-const CONFIG = {
-    url: "https://osofdmgokrxdzsxozsrf.supabase.co",
-    key: "sb_publishable_KTboch_e9KyCmMNZBlTM9w_V_XhY6Xe"
+window.SUPABASE_CONFIG = {
+  url: "https://osofdmgokrxdzsxozsrf.supabase.co",
+  key: "sb_publishable_KTboch_e9KyCmMNZBlTM9w_V_XhY6Xe"
 };
-
-// função global
-window.getConfig = function () {
-    return CONFIG;
-};
-
-</script>
-</head>
-<body></body>
-</html>
